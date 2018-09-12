84b5273c011ca2131252a37f72d549821d53128d
Chan Le <chanle@fb.com>
2018-09-12 13:54:33 -0700

    save user to db

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// DB STUFFS
+var MongoClient = require('mongodb').MongoClient
+var db = null;
+
+MongoClient.connect('mongodb://localhost:27017/main', function (err, client) {
+  if (err) throw err;
+  db = client.db('main');
+});
+// END DB STUFFS
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,12 +30,18 @@ app.get('/new', function (req, res) {
     let fname = req.query.first_name;
     let lname = req.query.last_name;
     let email = req.query.email;
-    
-    res.send(email);
+    const coll = db.collection('users');
+    coll.insert({fname: fname, lname: lname, email: email}, function(err, result) {
+        if (err) {
+            res.send('Error: ' + err);
+            return;
+        };
+        res.redirect('/quiz?id=' + result.ops[0]._id);
+    });
 });
 app.get('/quiz', function (req, res) {
-    let email = req.query.email;
-    res.send(email);
+    let id = req.query.id;
+    res.send(id);
 });
 app.listen(3000, () => console.log('App listening on port 3000!'));
 
